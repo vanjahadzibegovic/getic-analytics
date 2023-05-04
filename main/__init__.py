5bb9408faad79afea3b9491d6a8ac428b8a73bed
Vanja Hadzibegovic <vanja.hadzibegovic@gmail.com>
2023-05-04 23:04:58 +0200

    May 4th

--- main/__init__.py
+++ main/__init__.py
@@ -1,6 +1,7 @@
 from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
 from apscheduler.schedulers.background import BackgroundScheduler
+import os
 
 # from flask_bcrypt import Bcrypt
 
@@ -8,7 +9,7 @@ app = Flask(__name__)
 app.config[
     "SQLALCHEMY_DATABASE_URI"
 ] = "postgresql://postgres:smitSmece@localhost/getic_analytics_new"
-app.config["SECRET_KEY"] = "da293ksa082kda92"
+app.config["SECRET_KEY"] = os.urandom(32)
 db = SQLAlchemy(app)
 scheduler = BackgroundScheduler(daemon=True)
 # bcrypt = Bcrypt(app)
