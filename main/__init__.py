adbabac50d0a28ff923a907e7192d123402bcaa8
Vanja Hadzibegovic <vanja.hadzibegovic@gmail.com>
2023-08-29 14:28:12 +0200

    August 29

--- main/__init__.py
+++ main/__init__.py
@@ -5,11 +5,13 @@ from flask_login import LoginManager
 from apscheduler.schedulers.background import BackgroundScheduler
 from flask_mail import Mail
 import os
+from dotenv import load_dotenv
 
+load_dotenv()
 
 app = Flask(__name__)
-db_username = os.environ.get("DB_USER")
-db_password = os.environ.get("DB_PASS")
+db_username = os.environ["DB_USER"]
+db_password = os.environ["DB_PASS"]
 app.config[
     "SQLALCHEMY_DATABASE_URI"
 ] = f"postgresql://{db_username}:{db_password}@localhost/getic_analytics_new"
@@ -22,8 +24,8 @@ login_manager.login_message = None
 app.config["MAIL_SERVER"] = "smtp.zoho.eu"
 app.config["MAIL_PORT"] = 587
 app.config["MAIL_USE_TLS"] = True
-app.config["MAIL_USERNAME"] = os.environ.get("EMAIL_USER")
-app.config["MAIL_PASSWORD"] = os.environ.get("EMAIL_PASS")
+app.config["MAIL_USERNAME"] = os.environ["EMAIL_USER"]
+app.config["MAIL_PASSWORD"] = os.environ["EMAIL_PASS"]
 mail = Mail(app)
 scheduler = BackgroundScheduler(daemon=True)
 
--- main/views.py
+++ main/views.py
@@ -55,11 +55,14 @@ def send_reset_email(user):
         sender="getic_analytics@zohomail.eu",
         recipients=[user.email],
     )
+    # If you use Nginx replace "{url_for(...)}" part
+    # with "http://<web server static IP>/reset_password/{token}"
     msg.body = f"""To reset your password, visit the following link:
 {url_for('reset_token', token=token, _external=True)}.
 
 If you did not make this request then simply ignore this email and no changes will be made.
 """
+
     mail.send(msg)
     return msg.body
 
